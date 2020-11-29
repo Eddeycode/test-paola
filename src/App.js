11c51999d76e31c04efea0bd9078011603b94db8
you@example.com <tennouji.kotarou.kun@gmail.com>
2020-11-29 23:10:47 +0700

    Header mk.2

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
-import Header from './header/header.js';
+import Navbar from './header/Navbar.js';
+import Body from './pages/home.js';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <Header/>
-    </div>
+    <Router>
+      <Navbar/>
+      <Switch>
+        <Route path='/' exact />
+      </Switch>
+    </Router>
   );
 }
 
---
+++ src/header/Navbar.js
@@ -0,0 +1,47 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import './Navbar.css';
+import { Button } from '../components/Button';
+
+function Navbar() {
+  const [click, setClick] = useState(0);
+  const handleClick = () => setClick(!click);
+  const closeMenu = () => setClick(false);
+  return (
+    <div>
+      <>
+      <nav className="navbar">
+        <div className="navbar-container">
+            <div className='navbar-logo'>
+              <h1>Sarah</h1>
+            </div>  
+          <ul className= "nav-menu">
+            <li className='nav-item'>
+              <Link to='/about' className='nav-links' onClick={closeMenu}>
+                About
+              </Link>
+            </li>
+            <li className='nav-item'>
+              <Link to='/resume' className='nav-links' onClick={closeMenu}>
+                Resume
+              </Link>
+            </li>
+            <li className='nav-item'>
+              <Link to='/education' className='nav-links' onClick={closeMenu}>
+                Education
+              </Link>
+            </li>
+            <li className='nav-item'>
+              <Link to='/portofolio' className='nav-links' onClick={closeMenu}>
+                Portofolio
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </>
+    </div>
+  )
+}
+
+export default Navbar;
