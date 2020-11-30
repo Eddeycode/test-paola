0d4444f6ca897f3902411698d0dbf48755db6fab
you@example.com <tennouji.kotarou.kun@gmail.com>
2020-11-30 10:08:00 +0700

    fetching data from axios

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,60 +1,96 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import Button from '@material-ui/core/Button';
+import Card from '@material-ui/core/card';
+import CardContent from '@material-ui/core/CardContent'
+import axios from 'axios';
 import './Cards.css';
-import Typography from '@material-ui/core/Typography';
+import { Typography } from '@material-ui/core';
 
-const useStyles = makeStyles({
-  root: {
-    minWidth: 500,
-  },
-  bullet: {
-    display: 'inline-block',
-    margin: '0 2px',
-    transform: 'scale(0.8)',
-  },
-  title: {
-    fontSize: 14,
-  },
-  pos: {
-    marginBottom: 12,
-  },
-});
+class Cards extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      requestFailed: false,
+    }
+  }
 
-function Cards() {
+  componentDidMount() {
+    fetch('https://randomuser.me/api/').then((response) => {
+    return response.json()}).then((d) =>  {
+      console.log('parsed json', d)
+      this.setState({data: d});
+    }).catch(function(ex) {
+      console.log('parsing failed', ex)
+      this.setState({
+          requestFailed: true
+      })
+    })
+  }
 
-  return (
-    <Card className="root" variant="outlined">
-      <CardContent>
-        <Typography variant="h5" component="h2">
-          Nama
-        </Typography>
-        <Typography color="textSecondary" className="space">
-          Job
-        </Typography>
-        <Typography variant="body2" component="p">
-          DoB
-        </Typography>
-        <Typography variant="body2" component="p">
-          Gender
-        </Typography>
-        <Typography variant="body2" component="p">
-          Phone
-        </Typography>
-        <Typography variant="body2" component="p">
-          Email
-        </Typography>
-        <Typography variant="body2" component="p">
-          Address
-        </Typography>
-        <Typography variant="body2" component="p">
-          Location
-        </Typography>
-      </CardContent>
-    </Card>
-  );
+
+  render() {
+
+    if(this.state.requestFailed) return <p>Request failed.</p>
+    if(!this.state.data) return <p>Loading</p>
+
+    return (
+      <Card className="root" variant="outlined">
+        <CardContent>
+          <Typography variant="h5" component="h2">
+            {this.state.data.results[0].name.first} {this.state.data.results[0].name.last}
+          </Typography>
+          <Typography color="textSecondary" className="space">
+            Developer
+          </Typography>
+          <Typography className="spc" variant="body2" component ="p">
+            {this.state.data.results[0].dob.date}
+          </Typography>
+          <Typography className="spc" variant="body2" component ="p">
+            {this.state.data.results[0].phone}
+          </Typography>
+          <Typography className="spc" variant="body2" component ="p">
+            {this.state.data.results[0].email}
+          </Typography>
+          <Typography className="spc" variant="body2" component ="p">
+            {this.state.data.results[0].location.street.name} {this.state.data.results[0].location.street.number}, {this.state.data.results[0].location.city}, {this.state.data.results[0].location.state}, {this.state.data.results[0].location.country}
+          </Typography>
+          <Typography className="spc" variant="body2" component ="p">
+            GPS
+          </Typography>
+        </CardContent>
+      </Card>
+    );
+  }
 }
+
 export default Cards;
+
+
+  // return (
+  //   <Card className="root" variant="outlined">
+  //     <CardContent>
+  //       <Typography variant="h5" component="h2">
+  //         {persons.name};
+  //       </Typography>
+  //       <Typography color="textSecondary" className="space">
+  //         Job
+  //       </Typography>
+  //       <Typography variant="body2" component="p">
+  //         DoB
+  //       </Typography>
+  //       <Typography variant="body2" component="p">
+  //         Gender
+  //       </Typography>
+  //       <Typography variant="body2" component="p">
+  //         Phone
+  //       </Typography>
+  //       <Typography variant="body2" component="p">
+  //         Email
+  //       </Typography>
+  //       <Typography variant="body2" component="p">
+  //         Address
+  //       </Typography>
+  //       <Typography variant="body2" component="p">
+  //         Location
+  //       </Typography>
+  //     </CardContent>
+  //   </Card>
